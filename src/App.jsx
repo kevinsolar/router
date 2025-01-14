ef6a8a8656ff5258dcdc872d86ad25812081cef0
Kevin BTA <kevinleitesolar@hotmail.com>
2025-01-14 17:43:28 -0300

    Criando nosso menu de navegação com Link do React Router DOM, e estilizando nosso navbar.  Commit para pausa no curso.

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,22 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-
-import { useFetch } from "./hooks/useFetch";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Navbar from "./components/Navbar";
 
 const url = "http://localhost:3000/produtos";
 
 function App() {
-	const [produtos, setProdutos] = useState([]);
-	const [nome, setNome] = useState([]);
-	const [preco, setPreco] = useState([]);
-
-	const { data: itens, httpConfig, loading, error } = useFetch(url);
-
-	const handleSubmit = async (submit) => {
-		submit.preventDefault();
-
-		const produto = {
-			nome,
-			preco,
-		};
-		httpConfig(produto, "POST");
-
-		setNome("");
-		setPreco("");
-	};
-
-	const handleRemove = (id) => {
-		httpConfig(id, "DELETE");
-	};
-
 	return (
-		<main className="App">
-			<h1>Produtos</h1>
+		<div className="App">
          <BrowserRouter>
+            <Navbar />
             <Routes>
                <Route path="/" element={<Home />} />
+               <Route path="/sobre" element={<Sobre />} />
             </Routes>
          </BrowserRouter>
-
-			{loading && <p>Carregando...</p>}
-
-			{error && <p>{error}</p>}
-
-			{!error && (
-				<ul>
-					{itens &&
-						itens.map((produto) => (
-							<li key={produto.id}>
-								{produto.nome} - R$ {produto.preco}
-								<button onClick={() => handleRemove(produto.id)}>
-									Excluir
-								</button>
-							</li>
-						))}
-				</ul>
-			)}
-
-			<div className="add-produto">
-				<form onSubmit={handleSubmit}>
-					<label>
-						Nome:
-						<input
-							type="text"
-							value={nome}
-							name="nome"
-							onChange={(e) => setNome(e.target.value)}
-						/>
-					</label>
-					<label>
-						Preço:
-						<input
-							type="number"
-							value={preco}
-							name="preco"
-							onChange={(e) => setPreco(e.target.value)}
-						/>
-					</label>
-					{!loading && <input type="submit" value="Criar" />}
-					{loading && <input type="submit" disabled value="Aguarde" />}
-				</form>
-			</div>
-		</main>
+		</div>
 	);
 }
 
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+   return (
+      <nav>
+         <Link to="/">Home</Link>
+         <Link to="/sobre">Sobre</Link>
+      </nav>
+   )
+}
+
+export default Navbar;
