5b7cbac664977146da052e457026d05c1f84808d
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-15 23:38:24 -0300

    Adicionando carregamento dinâmico de dados para exibir informações do produto

--- src/pages/Produto/index.jsx
+++ src/pages/Produto/index.jsx
@@ -1,15 +1,34 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 import "./Produto.css";
 
 const Produto = () => {
-	// 4 - rota dinamica
-	const { id } = useParams();
+   // 4 - rota dinamica
+   /* Esse useParams nos traz um objeto com todos os parametros da URL,
+   como nesse caso nos sabemos oque queremos, entao podemos desestruturar com informacao que queremos. */
+   const { id } = useParams();
 
-	return (
-		<section>
-			<p>ID do produto: {id}</p>
-		</section>
-	);
+   // 5- Carregamento dado individual
+   const url = `http://localhost:3000/produtos/${id}`;
+   const { data: produto, loading, error } = useFetch(url);
+
+   console.log(produto);
+
+   return (
+      <section>
+         <p>ID do produto: {id}</p>
+
+         {error && <p>Ocorreu um erro</p>}
+         {loading && <p>Carregando...</p>}
+
+         {produto && (
+            <div>
+               <h1>{produto.nome}</h1>
+               <p>R$ {produto.preco}</p>
+            </div>
+         )}
+      </section>
+   );
 };
 
 export default Produto;
